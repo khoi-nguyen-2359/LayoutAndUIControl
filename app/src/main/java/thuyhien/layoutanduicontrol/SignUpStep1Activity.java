c31e1b585a2591ba0f79fbdc9483d8f274e038f3
ThuyHien261195 <thuyhien@MBA-Khoi.local>
2017-09-07 17:01:05 +0700

    Update ex 1.2

--- app/src/main/java/thuyhien/layoutanduicontrol/SignUpStep1Activity.java
+++ app/src/main/java/thuyhien/layoutanduicontrol/SignUpStep1Activity.java
@@ -3,24 +3,95 @@ package thuyhien.layoutanduicontrol;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class SignUpStep1Activity extends AppCompatActivity {
-    Button btnNext;
+    @BindView(R.id.editFirstName)
+    EditText editFirstName;
+
+    @BindView(R.id.editLastName)
+    EditText editLastName;
+
+    @BindView(R.id.editEmail)
+    EditText editEmail;
+
+    @BindView(R.id.editPhone)
+    EditText editPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_step1);
 
-        btnNext = (Button) findViewById(R.id.btnNext);
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), SignUpStep2Activity.class);
-                startActivity(intent);
-            }
-        });
+        ButterKnife.bind(this);
+
+    }
+
+    @OnTextChanged(R.id.editEmail)
+    public void onEmailChange(CharSequence cs, int start, int count, int after) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(editEmail.getText()).matches()) {
+            editEmail.setError(getResources().getString(R.string.warning_invalid,
+                    getResources().getString(R.string.email)));
+        }
+    }
+
+    @OnTextChanged(R.id.editPhone)
+    public void onPhoneChange(CharSequence cs, int start, int count, int after) {
+        if (!Patterns.PHONE.matcher(editPhone.getText()).matches()) {
+            editPhone.setError(getResources().getString(R.string.warning_invalid,
+                    getResources().getString(R.string.phone_number)));
+        }
+    }
+
+    @OnClick(R.id.btnNext)
+    public void onClickBtnNext() {
+        if (checkValidInput()) {
+            Intent intent = new Intent(getBaseContext(), SignUpStep2Activity.class);
+            startActivity(intent);
+            this.finish();
+        }
+        if (TextUtils.isEmpty(editFirstName.getText())) {
+            editFirstName.setError(getResources().getString(R.string.warning_enter,
+                    getResources().getString(R.string.first_name)));
+        }
+        if (TextUtils.isEmpty(editLastName.getText())) {
+            editLastName.setError(getResources().getString(R.string.warning_enter,
+                    getResources().getString(R.string.last_name)));
+        }
+        if (TextUtils.isEmpty(editEmail.getText())) {
+            editEmail.setError(getResources().getString(R.string.warning_enter,
+                    getResources().getString(R.string.email)));
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(editEmail.getText()).matches()) {
+            editEmail.setError(getResources().getString(R.string.warning_invalid,
+                    getResources().getString(R.string.email)));
+        }
+        if (TextUtils.isEmpty(editPhone.getText())) {
+            editPhone.setError(getResources().getString(R.string.warning_enter,
+                    getResources().getString(R.string.phone_number)));
+        } else if (!Patterns.PHONE.matcher(editPhone.getText()).matches()) {
+            editPhone.setError(getResources().getString(R.string.warning_invalid,
+                    getResources().getString(R.string.phone_number)));
+        }
+    }
+
+    public boolean checkValidInput() {
+        if (!TextUtils.isEmpty(editFirstName.getText()) &&
+                !TextUtils.isEmpty(editLastName.getText()) &&
+                !TextUtils.isEmpty(editEmail.getText()) &&
+                !TextUtils.isEmpty(editPhone.getText()) &&
+                Patterns.EMAIL_ADDRESS.matcher(editEmail.getText()).matches() &&
+                Patterns.PHONE.matcher(editPhone.getText()).matches()) {
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/thuyhien/layoutanduicontrol/SignUpStep2Activity.java
+++ app/src/main/java/thuyhien/layoutanduicontrol/SignUpStep2Activity.java
@@ -2,28 +2,56 @@ package thuyhien.layoutanduicontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SignUpStep2Activity extends AppCompatActivity {
+    @BindView(R.id.txtSalary)
     TextView txtSalary;
+
+    @BindView(R.id.seekBarSalary)
     SeekBar seekBarSalary;
+
+    @BindView(R.id.ckbFootball)
+    CheckBox ckbFootball;
+
+    @BindView(R.id.ckbTennis)
+    CheckBox ckbTennis;
+
+    @BindView(R.id.ckbPingPong)
+    CheckBox ckbPingPong;
+
+    @BindView(R.id.ckbSwimming)
+    CheckBox ckbSwimming;
+
+    @BindView(R.id.ckbVolleyball)
+    CheckBox ckbVolleyball;
+
+    @BindView(R.id.ckbBasketball)
+    CheckBox ckbBasketball;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_step2);
 
-        txtSalary = (TextView) findViewById(R.id.txtSalary);
-        seekBarSalary = (SeekBar) findViewById(R.id.seekBarSalary);
+        ButterKnife.bind(this);
+
+        // Set default value
+        txtSalary.setText(getResources().getString(R.string.your_salary, 0));
 
         seekBarSalary.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                String result = getResources().getString(R.string.your_salary) + ' ' +
-                        progress + getResources().getString(R.string.dollars);
+                String result = getResources().getString(R.string.your_salary, progress);
                 txtSalary.setText(result);
             }
 
@@ -37,5 +65,16 @@ public class SignUpStep2Activity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @OnClick(R.id.btnDone)
+    public void onClickBtnDone() {
+        if (!ckbFootball.isChecked() && !ckbTennis.isChecked() && !ckbPingPong.isChecked() &&
+                !ckbSwimming.isChecked() && !ckbVolleyball.isChecked() && !ckbBasketball.isChecked()) {
+            Toast.makeText(this, getResources().getString(R.string.warning_select_sport),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            this.finish();
+        }
     }
 }
